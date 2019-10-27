4bbc786b9f2befa84b1217637af19f42f1474343
Marcel Teixeira <marceltex@gmail.com>
2019-10-27 16:46:16 +0200

    Add icons

--- app/src/main/java/com/marceltex/recipeapp/ui/MainActivity.kt
+++ app/src/main/java/com/marceltex/recipeapp/ui/MainActivity.kt
@@ -2,10 +2,8 @@ package com.marceltex.recipeapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import com.marceltex.recipeapp.R
 import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +11,5 @@ class MainActivity : AppCompatActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar as? Toolbar)
     }
 }
