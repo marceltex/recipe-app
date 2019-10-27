d6019be65cbfaf6e6db0121335873eb1bce36d3f
Marcel Teixeira <marceltex@gmail.com>
2019-10-27 17:08:11 +0200

    Add animatiions for fragment transitions

--- app/src/main/java/com/marceltex/recipeapp/ui/BaseFragment.kt
+++ app/src/main/java/com/marceltex/recipeapp/ui/BaseFragment.kt
@@ -1,17 +1,33 @@
 package com.marceltex.recipeapp.ui
 
 import android.content.Context
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.airbnb.mvrx.BaseMvRxFragment
+import com.marceltex.recipeapp.R
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_base_mvrx.*
 
-abstract class BaseFragment : BaseMvRxFragment() {
+abstract class BaseFragment : BaseMvRxFragment(R.layout.fragment_base_mvrx) {
 
     open val title = ""
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        (toolbar as? Toolbar)?.setupWithNavController(findNavController())
     }
 
     override fun onResume() {
